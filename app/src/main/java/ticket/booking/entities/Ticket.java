d9c7dbc3823b8cb6288e9b1b9bf95f39a3f0e46b
masyudhossain <masyud.hossain2001@gmail.com>
2025-03-12 21:08:26 +0530

    update file

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -1,12 +1,31 @@
 package ticket.booking.entities;
 
-import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class Ticket {
     private String ticketId;
     private String userId;
     private String source;
     private String destination;
-    private Date dateOfTravel;
+    private String dateOfTravel;
     private Train train;
+
+    public Ticket(){}
+
+    public Ticket(String ticketId, String userId, String source, String destination, String dateOfTravel, Train train){
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", ticketId, userId, source, destination, dateOfTravel);
+    }
+
 }
--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -1,11 +1,60 @@
 package ticket.booking.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Setter;
+
 import java.util.List;
 
+@JsonNaming (PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
+
+    @Setter
     private String name;
     private String password;
-    private String hashPassword;
+    @Setter
+    private String hashedPassword;
+    @Setter
     private String userId;
-    private List<Ticket> ticketBooked;
+    @Setter
+    private List<Ticket> ticketsBooked;
+
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId){
+        this.name = name;
+        this.password = password;
+        this.hashedPassword = hashedPassword;
+        this.ticketsBooked = ticketsBooked;
+        this.userId = userId;
+    }
+    public User(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
+
+    public void printTickets(){
+        for (int i = 0; i<ticketsBooked.size(); i++){
+            System.out.println(ticketsBooked.get(i).getTicketInfo());
+        }
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
 }
+
